a35123326d5dd182ac744cf6fa750723e6496942
MohamedMohamed <m01126165016@icloud.com>
2024-05-24 22:56:50 +0300

    Install RXJava

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation (libs.rxjava)
     implementation(libs.rxkotlin)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/rxjava/MainActivity.kt
+++ app/src/main/java/com/example/rxjava/MainActivity.kt
@@ -16,5 +16,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        foo()
+    }
+
+    private fun foo() {
     }
 }
